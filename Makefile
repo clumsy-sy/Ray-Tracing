adc0426bcf11b42393afb7789d064159b7ce843c
clumsy-sy <1725798537@qq.com>
2023-08-12 10:25:41 +0800

    update: use makefile and add some LOG

--- Makefile
+++ Makefile
@@ -1,11 +1,54 @@
+CC = g++
+CFLAGS = -O3 -march=native -std=c++20 -Wall -Wextra
 
+SRC = $(wildcard src/*.cu)
+SRC += $(wildcard src/vector/*.cu)
+OBJ = $(patsubst %.cu, %.o, ${SRC})
 
-pre-build:
-	g++ -c src/external/rtw_stb_image.cpp -o build/stb_image.o -O3 -march=native -std=c++20  -Wall -Wextra
-	g++ src/vector/Vec3dx4.hpp -O3 -march=native -std=c++20  -Wall -Wextra
-	g++ src/global.hpp -O3 -march=native -std=c++20  -Wall -Wextra
-	g++ src/external/OBJ_Loader.hpp -O3 -march=native -std=c++20  -Wall -Wextra
+TARGET := build/RayTracing
 
-build:
-	g++ -c src/main.cpp -o build/RayTracing.o -O3 -march=native -std=c++20  -Wall -Wextra
-	g++ build/RayTracing.o build/stb_image.o -o build/RayTracing
+.PHONY : pbuild
+pbuild : 
+	$(CC) -c src/external/rtw_stb_image.cpp -o build/stb_image.o $(CFLAGS)
+	$(CC) src/vector/Vec3dx4.hpp $(CFLAGS)
+	$(CC) src/global.hpp $(CFLAGS)
+	$(CC) src/external/OBJ_Loader.hpp $(CFLAGS)
+
+.PHONY : build
+build : 
+	$(CC) -c src/main.cpp -o build/RayTracing.o $(CFLAGS)
+	$(CC) build/RayTracing.o build/stb_image.o -o build/RayTracing
+
+.PHONY : run
+run : $(TARGET)
+	./$(TARGET)
+
+.PHONY : cmrun
+cmrun:
+	cmake --build build --config Release --parallel $(nproc)
+	./$(TARGET)
+
+.PHONY : perf
+perf: $(TARGET)
+	nvprof ./$(TARGET)
+
+.PHONY : build
+build: $(TARGET)
+
+.PHONY : fmt
+fmt:
+	clang-format -i $(shell find src -name '*.cpp')
+	clang-format -i $(shell find src -name '*.hpp')
+
+.PHONY : clean
+clean:
+	rm src/*.o
+
+.PHONY : buildclean
+buildclean:
+	cd src/build && rm -rf *
+
+.PHONY : convert
+convert:
+	convert  xxx.jpg  xxx.png
+
--- src/camera/camera.hpp
+++ src/camera/camera.hpp
@@ -3,6 +3,15 @@
 
 #include "camerabase.hpp"
 
+/*
+  camerabase {
+    point3 origin ; 
+    point3 lower_left_corner;
+    Vec3d horizontal, vertical;
+  }
+  camera 是一个有透镜的摄像机
+*/
+
 class camera : public camerabase {
 public:
   Vec3d u, v, w;      // w:看向方向，u：镜头平面的 x，v：镜头屏幕的 y
@@ -11,9 +20,10 @@ public:
 public:
   camera() = default;
   camera(const point3 &lookfrom, const point3 &lookat, const Vec3d &vup, double vfov, double aspect_ratio,
-      double aperture, double focus_dist) {
+      double aperture) {
     auto theta = degrees_to_radians(vfov);
     auto h = tan(theta / 2);
+    auto focus_dist = (lookfrom - lookat).length();
     auto viewport_height = 2.0 * h;
     auto viewport_width = aspect_ratio * viewport_height;
 
@@ -27,10 +37,25 @@ public:
     lower_left_corner = origin - horizontal / 2 - vertical / 2 - focus_dist * w;
 
     lens_radius = aperture / 2;
+    #ifdef LOG
+      printf("[Cam LOG] : lookfrom[x: %5.3lf  y: %5.3lf  z: %5.3lf]\
+  -> lookat[x: %5.3lf  y: %5.3lf  z: %5.3lf] vup[x: %5.3lf  y: %5.3lf  z: %5.3lf]\n",
+       lookfrom[0], lookfrom[1], lookfrom[2], lookat[0], lookat[1], lookat[2], vup[0], vup[1], vup[2]);
+      printf("          | lookdir[x: %5.3lf  y: %5.3lf  z: %5.3lf],\
+  xdir[x: %5.3lf  y: %5.3lf  z: %5.3lf], ydir[x: %5.3lf  y: %5.3lf  z: %5.3lf]\n",
+        w[0], w[1], w[2], u[0], u[1], u[2], v[0], v[1], v[2]);
+      printf("          | horizontal[x: %5.3lf  y: %5.3lf  z: %5.3lf],\
+  vertical[x: %5.3lf  y: %5.3lf  z: %5.3lf], lower_left_corner[x: %5.3lf  y: %5.3lf  z: %5.3lf]\n",
+        horizontal[0], horizontal[1], horizontal[2], vertical[0], vertical[1], vertical[2], 
+        lower_left_corner[0], lower_left_corner[1], lower_left_corner[2]);
+      printf("          | vfov: %5.3lf, theta: %5.3lf,h: %5.3lf, viewport_height: %5.3lf, viewport_width: % 5.3f\n", 
+      vfov, theta, h, viewport_height, viewport_width);
+      printf("----------------------------------------\n");
+    #endif
   }
   [[nodiscard]] auto get_ray(double s, double t) const -> ray override {
-    Vec3d rd = lens_radius * random_in_unit_disk(); // 镜头中随机一个点
-    Vec3d offset = u * rd.x() + v * rd.y();
+    Vec3d rd = lens_radius * random_in_unit_disk(); 
+    Vec3d offset = u * rd.x() + v * rd.y();         // 镜头的光圈中随机一个点
 
     return {origin + offset, lower_left_corner + s * horizontal + t * vertical - origin - offset};
   }
