88f9de7503bc8281630727963f6c186b1ae21576
clumsy-sy <clumsysysy@gmail.com>
2023-12-07 10:33:31 +0800

    update zoo scene

--- src/scene/zoo.hpp
+++ src/scene/zoo.hpp
@@ -10,6 +10,7 @@
 #include "../geometry/quad.hpp"
 #include "../material/diffuse_light.hpp"
 #include "../material/lambertian.hpp"
+#include "../material/metal.hpp"
 #include "../texture/image_texture.hpp"
 #include <memory>
 
@@ -74,18 +75,23 @@ auto cornell_box_bunny_rotate(hittable_list &world, hittable_list &light) -> voi
   world.add(std::make_unique<yz_rect>(0, 555, 0, 555, 555, green));
   world.add(std::make_unique<yz_rect>(0, 555, 0, 555, 0, red));
   auto m = nullptr;
-  light.add(std::make_unique<quad>(point3(343, 554, 332), vec3d(-130, 0, 0), vec3d(0, 0, -105), m));
-  world.add(std::make_unique<quad>(point3(343, 554, 332), vec3d(-130, 0, 0), vec3d(0, 0, -105), wlight));
+  light.add(std::make_unique<quad>(point3(343, 554, 232), vec3d(-130, 0, 0), vec3d(0, 0, -105), m));
+  world.add(std::make_unique<quad>(point3(343, 554, 232), vec3d(-130, 0, 0), vec3d(0, 0, -105), wlight));
   world.add(std::make_unique<xz_rect>(0, 555, 0, 555, 0, white));
   world.add(std::make_unique<xz_rect>(0, 555, 0, 555, 555, white));
   world.add(std::make_unique<xy_rect>(0, 555, 0, 555, 555, white));
 
-  // auto aluminum = new metal(color(0.8, 0.85, 0.88), 0.0);
-  // auto box1 = new box(point3(0,0,0), point3(165,330,165), aluminum);
+
+
   auto box1 = std::make_unique<translate>(
       std::make_unique<rotate_y>(std::make_unique<box>(point3(0, 0, 0), point3(165, 330, 165), white), 15),
       vec3d(265, 0, 295));
   world.add(std::move(box1));
+  // auto aluminum = new metal(color(0.8, 0.85, 0.88), 0.0);
+  // auto box1 = std::make_unique<translate>(
+  //     std::make_unique<rotate_y>(std::make_unique<box>(point3(0, 0, 0), point3(165, 330, 165), aluminum), 15),
+  //     vec3d(265, 0, 295));
+  // world.add(std::move(box1));
 
   auto box2 = std::make_unique<translate>(
       std::make_unique<rotate_y>(std::make_unique<box>(point3(0, 0, 0), point3(165, 165, 165), white), -18),
